1c40bf64f9c6386a7c66e928a24e5a68d79c85ad
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2013-08-23 02:22:26 -0400

    Parser.java - Added comments

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -13,15 +13,16 @@ public class Parser {
 		Document doc;
 		
 		try{
-			
+			//uses Jsoup.connect to retrieve the url from where the elements to be extracted
 			doc = Jsoup.connect("http://www.transitbc.com/regions/cfv/schedules/schedule.cfm?rmnu=dvSchedule&line=1&").get();
-			String title = doc.title();
-			Elements links = doc.select("td");
+			String title = doc.title(); //gets title of the url (webpage)
+			Elements links = doc.select("td"); //selects all the td elements in the page
 			System.out.println("Title" +title);
 			
 			for(Element table:links)
 			{
-				if(table.attr("class").equals("css-sched-times"))
+				//if the class of td is css-sched-times then it prints the text within those td's
+				if(table.attr("class").equals("css-sched-times")) 
 				{
 					System.out.print(table.text() + "\n");
 				}
